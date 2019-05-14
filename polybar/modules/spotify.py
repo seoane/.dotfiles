a1c409ee19a8e2edbfa075d7909ec004b4caab40
JESUS SEOANE VILA <suso.seoane@keepler.io>
2019-05-14 09:12:32 +0200

    add verbose debug

--- polybar/modules/spotify.py
+++ polybar/modules/spotify.py
@@ -27,7 +27,7 @@ def get_song():
         print song
         return output
     except:
-        print("")
+        print("No Song")
     pass
 
 
@@ -46,7 +46,7 @@ def action(action):
         elif action == 'previous':
             interface.Previous()
     except Exception as e:
-        print(e)
+        print(e + "action")
 
 
 def main():
